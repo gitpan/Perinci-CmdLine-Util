3c8d8137ec2eb7298b940b43b317dfe01a62d052
perlancar <perlancar@gmail.com>
2014-11-01 09:21:00 +0000

    Import of PERLANCAR/Perinci-CmdLine-Util-0.02 from CPAN.
    gitpan-cpan-distribution: Perinci-CmdLine-Util
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         PERLANCAR/Perinci-CmdLine-Util-0.02.tar.gz
    gitpan-cpan-author:       PERLANCAR
    gitpan-cpan-maturity:     released

--- lib/Perinci/CmdLine/Util.pm
+++ lib/Perinci/CmdLine/Util.pm
@@ -1,7 +1,7 @@
 package Perinci::CmdLine::Util;
 
 our $DATE = '2014-11-01'; # DATE
-our $VERSION = '0.01'; # VERSION
+our $VERSION = '0.02'; # VERSION
 
 use 5.010001;
 use strict;
@@ -22,12 +22,16 @@ The criteria are:
 
 * the file must exist and readable;
 
-* (optional, turned on by `filter_x`) file must have its executable mode bit
-  set;
+* (optional, if `include_noexec` is false) file must have its executable mode
+  bit set;
 
 * content must start with a shebang C<#!>;
 
-* content must contain something like `use Perinci::CmdLine`;
+* either: must be perl script (shebang line contains 'perl') and must contain
+  something like `use Perinci::CmdLine`;
+
+* or: a script tagged as a wrapper script and the wrapped script is a
+  Perinci::CmdLine script.
 
 _
     args => {
@@ -36,9 +40,42 @@ _
             req => 1,
             pos => 0,
         },
-        filter_x => {
-            summary => 'If on, only consider script that has +x mode bit set',
+        include_noexec => {
+            summary => 'Include scripts that do not have +x mode bit set',
             schema  => 'bool*',
+            default => 1,
+        },
+        include_backup => {
+            summary => 'Include backup files',
+            schema  => 'bool*',
+            default => 0,
+        },
+        include_wrapper => {
+            summary => 'Include wrapper scripts',
+            description => <<'_',
+
+A wrapper script is another Perl script, a shell script, or some other script
+which wraps a Perinci::CmdLine script. For example, if `list-id-holidays` is a
+Perinci::CmdLine script, then this shell script called `list-id-joint-leaves` is
+a wrapper:
+
+    #!/bin/bash
+    list-id-holidays --is-holiday=0 --is-joint-leave=0 "$@"
+
+It makes sense to provide the same completion for this wrapper script as
+`list-id-holidays`.
+
+To help this function detect such script, you need to put a tag inside the file:
+
+    #!/bin/bash
+    # TAG wrapped=list-id-holidays
+    list-id-holidays --is-holiday=0 --is-joint-leave=0 "$@"
+
+If this option is enabled, these scripts will be included.
+
+_
+            schema  => 'bool*',
+            default => 0,
         },
     },
 };
@@ -46,12 +83,19 @@ sub detect_perinci_cmdline_script {
     my %args = @_;
 
     my $script = $args{script} or return [400, "Please specify script"];
+    my $include_noexec  = $args{include_noexec}  // 1;
+    my $include_backup  = $args{include_backup}  // 0;
+    my $include_wrapper = $args{include_wrapper} // 0;
 
     my $yesno = 0;
     my $reason = "";
 
   DETECT:
     {
+        if (!$include_backup && $script =~ /(~|\.bak)$/) {
+            $reason = "Backup filename is excluded";
+            last;
+        }
         unless (-f $script) {
             $reason = "Not a file";
             last;
@@ -71,18 +115,58 @@ sub detect_perinci_cmdline_script {
             last;
         }
         my $shebang = <$fh>;
-        unless ($shebang =~ /perl/) {
-            $reason = "Does not have 'perl' in the shebang line";
-            last;
-        }
-        while (<$fh>) {
-            if (/^\s*(use|require)\s+Perinci::CmdLine(|::Any|::Lite)/) {
-                $yesno = 1;
-                last DETECT;
+
+        for my $alt (1..2) {
+            # detect Perinci::CmdLine script
+            {
+                last unless $alt==1;
+                unless ($shebang =~ /perl/) {
+                    $reason = "Does not have 'perl' in the shebang line";
+                    last;
+                }
+                while (<$fh>) {
+                    if (/^\s*(use|require)\s+Perinci::CmdLine(|::Any|::Lite)/) {
+                        $yesno = 1;
+                        last DETECT;
+                    }
+                }
+                $reason = "Can't find any statement requiring Perinci::CmdLine".
+                    " module family";
             }
-        }
-        $reason = "Can't find any statement requiring Perinci::CmdLine ".
-            "module family";
+            # detect wrapper script
+          DETECT_WRAPPER:
+            {
+                last unless $alt==2;
+                last unless $include_wrapper;
+                seek $fh, 0, 0;
+                # XXX currently simplistic
+                while (<$fh>) {
+                    if (/^# TAG wrapped=([^=\s]+)\s*$/) {
+                        require File::Which;
+                        my $path = File::Which::which($1);
+                        if (!$path) {
+                            $reason = "Tagged as wrapper but ".
+                                "wrapped program '$1' not found in PATH";
+                            last DETECT_WRAPPER;
+                        }
+                        my $res = detect_perinci_cmdline_script(
+                            script          => $path,
+                            include_backup  => $include_backup,
+                            include_noexec  => $include_noexec,
+                            include_wrapper => 0, # currently not recursive
+                        );
+                        if ($res->[0] != 200 || !$res->[2]) {
+                            $reason = "Tagged as wrapper but wrapped program ".
+                                "'$1' is not a Perinci::CmdLine script";
+                        }
+                        $yesno = 1;
+                        $reason = "Wrapper script for '$1'";
+                        last DETECT;
+                    }
+                }
+                $reason = "Can't find wrapper tag";
+            }
+        } # for alt
     }
 
     [200, "OK", $yesno, {"func.reason"=>$reason}];
@@ -103,7 +187,7 @@ Perinci::CmdLine::Util - Utility routines related to Perinci::CmdLine
 
 =head1 VERSION
 
-This document describes version 0.01 of Perinci::CmdLine::Util (from Perl distribution Perinci-CmdLine-Util), released on 2014-11-01.
+This document describes version 0.02 of Perinci::CmdLine::Util (from Perl distribution Perinci-CmdLine-Util), released on 2014-11-01.
 
 =head1 SYNOPSIS
 
@@ -122,12 +206,16 @@ The criteria are:
 
 =item * the file must exist and readable;
 
-=item * (optional, turned on by C<filter_x>) file must have its executable mode bit
-set;
+=item * (optional, if C<include_noexec> is false) file must have its executable mode
+bit set;
 
 =item * content must start with a shebang C<#!>;
 
-=item * content must contain something like C<use Perinci::CmdLine>;
+=item * either: must be perl script (shebang line contains 'perl') and must contain
+something like C<use Perinci::CmdLine>;
+
+=item * or: a script tagged as a wrapper script and the wrapped script is a
+Perinci::CmdLine script.
 
 =back
 
@@ -135,9 +223,36 @@ Arguments ('*' denotes required arguments):
 
 =over 4
 
-=item * B<filter_x> => I<bool>
+=item * B<include_backup> => I<bool> (default: 0)
 
-If on, only consider script that has +x mode bit set.
+Include backup files.
+
+=item * B<include_noexec> => I<bool> (default: 1)
+
+Include scripts that do not have +x mode bit set.
+
+=item * B<include_wrapper> => I<bool> (default: 0)
+
+Include wrapper scripts.
+
+A wrapper script is another Perl script, a shell script, or some other script
+which wraps a Perinci::CmdLine script. For example, if C<list-id-holidays> is a
+Perinci::CmdLine script, then this shell script called C<list-id-joint-leaves> is
+a wrapper:
+
+ #!/bin/bash
+ list-id-holidays --is-holiday=0 --is-joint-leave=0 "$@"
+
+It makes sense to provide the same completion for this wrapper script as
+C<list-id-holidays>.
+
+To help this function detect such script, you need to put a tag inside the file:
+
+ #!/bin/bash
+ # TAG wrapped=list-id-holidays
+ list-id-holidays --is-holiday=0 --is-joint-leave=0 "$@"
+
+If this option is enabled, these scripts will be included.
 
 =item * B<script>* => I<any>
 
